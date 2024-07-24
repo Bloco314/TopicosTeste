c9ffc9307c0f7c3fd2f0041fb68a82a702ad1dfc
Erik Felipe(PC casa) <erikviana32@gmail.com>
2024-07-24 19:17:52 -0400

    new brach

---
+++ cypress/e2e/transations.js
@@ -0,0 +1,30 @@
+const adress = "https://parabank.parasoft.com/parabank/index.htm";
+
+export function fundsTransference(amount){
+    cy.get("a").contains("transfer").click();
+    cy.get('input[id="amount"]').type(amount);
+
+    cy.get("input[type=submit]").contains("Transfer").click();
+}
+
+export function searchTransationByDate(date){
+    cy.get("a").contains("findtrans").click();
+    cy.get('input[id="transactionDate"]').type(date);
+
+    cy.get("input[type=submit]").contains("findByDate").click();
+}
+
+export function searchTransationByAmount(amount){
+    cy.get("a").contains("findtrans").click();
+    cy.get('input[id="amount"]').type(amount);
+
+    cy.get("input[type=submit]").contains("findByAmount").click();
+}
+
+export function searchTransationByDateRange(initialDate, finalDate){
+    cy.get("a").contains("findtrans").click();
+    cy.get('input[id="fromDate"]').type(initialDate);
+    cy.get('input[id="toDate"]').type(finalDate);
+
+    cy.get("input[type=submit]").contains("findByDateRange").click();
+}
